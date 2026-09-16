5402c0cfc9557c50b8abd00a71ff80c0cdad09f5
haadimn <naqvihaadi@gmail.com>
2026-09-16 22:17:55 +0200

    Add kaplay-plugin-tiled for enhanced tilemap support

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import kaplay from "kaplay";
+import { tiledPlugin } from "kaplay-plugin-tiled";
 import { room1 } from "./scenes/room1.js";
 
-const k = kaplay();
+const k = kaplay({ plugins: [tiledPlugin] });
 
 room1(k);
 k.go("room1");
