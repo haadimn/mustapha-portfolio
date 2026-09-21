5090de3a17fd32507e5a6a25648d688989f687c3
haadimn <naqvihaadi@gmail.com>
2026-09-21 15:04:09 +0200

    Update DJ setup configuration: add Linktree icon and adjust tile layout

--- src/floors/config/about.js
+++ src/floors/config/about.js
@@ -1,5 +1,6 @@
 import karachiGif from "../../../art/8bit karachi.gif";
 import soundcloudIcon from "../../../art/soundcloud-ar21.svg";
+import linktreeIcon from "../../../art/linktree-seeklogo-2.svg";
 
 export const propConfig = {
   window: { size: "oneliner", gif: karachiGif },
@@ -7,7 +8,10 @@ export const propConfig = {
 
 export const dashboardConfig = {
   "dj-setup": {
-    cols: 1,
-    tiles: [{ image: soundcloudIcon, label: "SoundCloud" }],
+    cols: 2,
+    tiles: [
+      { image: soundcloudIcon, label: "SoundCloud" },
+      { image: linktreeIcon, label: "Linktree" },
+    ],
   },
 };
